4d8f31962a81c90cd971311ac9c0d4aa19943e12
Lareolan <konstantin_koton@yahoo.com>
2014-09-11 12:22:13 -0400

    Added jQuery support to the project

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,13 @@
-﻿
+﻿/// <reference path="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" />
+
+
+console.log("Hello World!");
+
+if (jQuery) {
+    console.log("jQuery is active");
+}
+
+$(document).ready({
+
+});
+
